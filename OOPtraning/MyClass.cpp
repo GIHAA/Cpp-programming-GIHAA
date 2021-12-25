6cdb9a3d8d896e7e8b04e54c12b17974c2441d77
GIHAA <gihansudeeparandimal@gmail.com>
2021-12-26 03:29:14 +0530

    up

---
+++ OOPtraning/MyClass.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+
+int add (int x, int y){
+
+  return x + y; 
+}
+
+int sub( int x , int y){
+
+  return  x - y;
+
+} 
+
+void displayname(char name [10]){
+
+    std::cout<<name;
+
+}
+
+
+
+//So, basically, MyClass::MyClass() refers to the MyClass() member function - or, in this case, constructor - of the MyClass class.
+
---
+++ OOPtraning/MyClass.h
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int add (int x, int y);
+
+int sub( int x , int y);
+
+void displayname( char name [10]);
+
