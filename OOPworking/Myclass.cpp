27be81550ca62491a530a63a043ab4c7f4bffc06
Gihan sudeepa <86099252+GIHAA@users.noreply.github.com>
2021-12-26 03:32:29 +0530

    Create Myclass.cpp

---
+++ OOPworking/Myclass.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+int add (int x, int y){
+
+  return x + y; 
+}
+
+int sub( int x , int y){
+
+  return  x - y;
+
+} 
+
+void displayname(char name [10]){
+
+    std::cout<<name;
+
+}
+
