8f339c74ea4c2e762f8eb86789ca129321c33375
GIHAA <gihansudeeparandimal@gmail.com>
2021-12-25 18:46:01 +0530

    up

---
+++ hello world/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main( void ){
+
+    int n1 = 0 ,n2 = 0 , n3 = 0;
+    cin >> n1 >> n2 >> n3;
+    cout << "\n\n" << endl;
+    cout << n1 <<"\n" << n2 << "\n"<< n3;
+
+    return 0;
+}
+
