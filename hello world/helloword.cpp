8386a29539037251e0d1c8e9266e28d5a46cdb4c
GIHAA <gihansudeeparandimal@gmail.com>
2021-12-25 15:32:28 +0530

    changes

--- hello world/helloword.cpp
+++ hello world/helloword.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
 int main(){
+    int num;
+    float num2;
+    double num3;
+    char name[10];
+    char c;
 
-    std::cout << "Hello world !";
-    std::cout << std::endl;
+    using namespace std;
+    // using std::cout;
+    //using std::cin;
+   // using std::endl;
+
+    for( int i = 0; i  <5; i++){
+
+        cout << "Hello \n";
+
+    }
+
     return 0;
+
 }
