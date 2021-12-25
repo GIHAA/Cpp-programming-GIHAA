1076fb3626e7a5d2e3c74e7a6823aa04dbcc8bf7
Gihan sudeepa <86099252+GIHAA@users.noreply.github.com>
2021-12-26 03:32:01 +0530

    Create main.cpp

---
+++ OOPworking/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "MyClass.h"
+
+int main(){
+
+  char name[10] = {"GIHAN"};
+  std::cout<<add(20, 20)<<std::endl;
+  std::cout<<sub(20, 20)<<std::endl;
+  displayname(name);
+
+}
